e875c25d2ce998bb803ec3fec230fcba0ae2d446
HectorMtz22 <hec.gre81@gmail.com>
2021-07-12 20:04:22 -0500

    Alloc

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,27 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import Constants from "expo-constants";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import { Header } from "./components/Header";
 
 export default function Main() {
   return (
-    <View style={styles.container}>
+    <View>
+      <StatusBar backgroundColor="#000" style="light" />
       <Header />
-      <Text style={styles.text}>Header!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Constants.statusBarHeight,
-    paddingVertical: 5,
-    paddingHorizontal: 30,
-    backgroundColor: "#fff",
-  },
-  text: {
-    fontSize: 20,
-  },
-});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
 
 export const Header = () => {
   return (
-    <View>
-      <Text>This is a Header!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>This is a Header!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    paddingVertical: 5,
+    paddingHorizontal: 30,
+    backgroundColor: "#000",
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
+  },
+});
