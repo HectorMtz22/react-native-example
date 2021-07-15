edff57c8755c08eeb7dfc0e79e741f9039061724
HectorMtz22 <hec.gre81@gmail.com>
2021-07-14 22:40:49 -0500

    Theme

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: "#349598",
+    secondary: "#124B4D",
+    active: "#65E1E5",
+    constrast: "#4D240B",
+    contrastClear: "#995B34",
+  },
+  fontSizes: {
+    body: 12,
+  },
+};
+
+export default theme;
